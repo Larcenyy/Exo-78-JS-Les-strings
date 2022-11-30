d8f2064e861e93ab35cf108d6fc442f483ef110f
Rémy <remyc59440@hotmail.com>
2022-11-30 15:03:00 +0100

    exo 78 fini

--- exercice1.js
+++ exercice1.js
@@ -1,10 +1,19 @@
 let monTexte = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua";
-
+let span = document.getElementById("position");
 // Completer le code suivant pour afficher la longueur de la variable monTexte
 
-alert(monTexte.);
+
+console.log(monTexte.indexOf('i'));
+console.log(monTexte.length);
 
 
+span.innerHTML = monTexte.toUpperCase()
+
+let maLettre = monTexte[22];
+
+for(let i = 0; i < maLettre.length; i++){
+    console.log(maLettre[i]);
+}
 //Ajouter du code ci dessous pour réaliser la suite de l'exercice
 
 
